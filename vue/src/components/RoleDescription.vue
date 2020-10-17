cb47ed8d1e52045cb92277bbb974716dd2b44438
funasaka <c501306014@gmail.com>
2020-10-17 16:36:31 +0900

    topページを作成

---
+++ vue/src/components/RoleDescription.vue
@@ -0,0 +1,67 @@
+<template>
+    <div>
+      <h2>役職紹介</h2>  
+      <div v-for="role in RoleList" v-bind:key=role>     
+          <div>
+            <img v-bind:src='role.img'>
+            
+          </div>  
+          <div>
+            <h3>{{ role.name }}</h3> 
+          </div>
+          <div>
+            {{ role.description }}
+          </div>
+      </div>
+    </div>
+    
+</template>
+
+<script>
+export default {
+    name:"RoleDescription",
+    data(){
+        return{
+            RoleList:[
+                {
+                    name: "人狼",
+                    img: require("../assets/chara/chara1.png"),
+                    description: "人狼です"
+                },
+                {
+                    name: "村人",
+                    img: require("../assets/chara/chara2.png"),
+                    description: "村人です"
+                },
+                {
+                    name: "占い師",
+                    img: require("../assets/chara/chara3.png"),
+                    description: "占い師です"
+                },
+                {
+                    name: "怪盗",
+                    img: require("../assets/chara/chara4.png"),
+                    description: "怪盗です"
+                },
+                {
+                    name: "狂人",
+                    img: require("../assets/chara/chara5.png"),
+                    description: "狂人です"
+                },
+                {
+                    name: "吊り人",
+                    img: require("../assets/chara/chara6.png"),
+                    description: "釣り人です"
+                }
+            ]
+        }
+    },
+}
+</script>
+
+<style>
+img {
+  width: 100px;
+  height: 100px;
+}
+</style>
---
+++ vue/src/views/TopPage.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="TopPage">
+    <div>
+    <h1>ワンナイト人狼</h1>  
+    </div>
+
+    <div>
+      <button>ルームを作成</button>
+      <button>ルームに参加</button>
+    </div>    
+
+    <div>
+      <p>
+        テキストテキストテキストテキストテキスト
+      </p>
+    </div>
+    <div>
+      <h2>
+        ワンナイト人狼とは？
+      </h2>  
+      <p>
+        テキストテキストテキストテキストテキスト
+      </p>
+    </div>
+
+    <RoleDescription/>
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import RoleDescription from '@/components/RoleDescription.vue'
+
+export default {
+  name: 'TopPage',
+  components:{
+    RoleDescription
+  }
+  }
+</script>

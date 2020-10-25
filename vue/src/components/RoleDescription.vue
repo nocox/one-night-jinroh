652c4637ed64436d68e403caac1f784ef2caf389
funasaka <c501306014@gmail.com>
2020-10-25 22:23:36 +0900

    役職紹介にスタイルを適用

--- vue/src/components/RoleDescription.vue
+++ vue/src/components/RoleDescription.vue
@@ -1,18 +1,21 @@
 <template>
     <div>
-      <h2>役職紹介</h2>  
-      <div v-for="role in RoleList" v-bind:key=role>     
-          <div>
-            <img v-bind:src='role.img'>
-            
-          </div>  
-          <div>
-            <h3>{{ role.name }}</h3> 
-          </div>
-          <div>
-            {{ role.description }}
-          </div>
-      </div>
+        <h2>役職紹介</h2>  
+        <div class='RoleList'>
+            <div class='Role' v-for="role in RoleList" v-bind:key=role>     
+                <div class='RoleImage'>
+                    <img v-bind:src='role.img'>
+                </div> 
+                <div>
+                    <div class='RoleName'>
+                        <h3>{{ role.name }}</h3> 
+                    </div>
+                    <div class='RoleDescription'>
+                        {{ role.description }}
+                    </div>
+                </div> 
+            </div>          
+        </div>
     </div>
     
 </template>
@@ -26,12 +29,12 @@ export default {
                 {
                     name: "人狼",
                     img: require("../assets/chara/chara1.png"),
-                    description: "人狼です"
+                    description: "人狼です人狼です人狼です人狼です人狼です人狼です人狼です人狼です"
                 },
                 {
                     name: "村人",
                     img: require("../assets/chara/chara2.png"),
-                    description: "村人です"
+                    description: "村人です村人です村人です村人です村人です村人です村人です村人です"
                 },
                 {
                     name: "占い師",
@@ -64,4 +67,27 @@ img {
   width: 100px;
   height: 100px;
 }
+
+.RoleList{
+    width:720px;
+    display: flex;
+    margin:0 auto;
+    flex-wrap: wrap;
+    
+}
+
+.Role{
+    width:50%;
+    display:flex;
+}
+
+.RoleName{
+    text-align: left;
+}
+
+.RoleDescription{
+    text-align: left;
+}
+
+
 </style>

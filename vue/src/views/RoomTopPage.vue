4b77f7ef0ff0bf954f13907cd59c43e4cdb7cede
c501306014 <c501306014@gmail.com>
2020-11-21 16:58:57 +0900

    ルームトップページの中身を進めた

--- vue/src/views/RoomTopPage.vue
+++ vue/src/views/RoomTopPage.vue
@@ -1,14 +1,46 @@
 <template>
-    <GameDescription />
+    <div>
+        <IsMatch_rule_players />
+
+        <div id="RoomNum">
+            部屋番号：
+        </div>
+
+        <div>
+            参加プレイヤー表示エリア
+        </div>        
+        
+        <div>
+            ルール設定エリア
+        </div>        
+        
+        <div>
+            準備完了エリア
+        </div>        
+        
+        <GameDescription />
+    </div>
 </template>
 
 <script>
 import GameDescription from "@/components/GameDescription.vue"
+import IsMatch_rule_players from "@/components/IsMatch_rule_players.vue"
 
 export default {
     name: "RoomTopPage",
     components:{
-        GameDescription
+        GameDescription,
+        IsMatch_rule_players
     }
 }
 </script>
+
+<style>
+
+
+    #RoomNum{
+        border: solid 1px #50A0F6;
+        width:20rem;
+        margin: 1rem auto;
+    }
+</style>

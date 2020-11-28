4f3d5549ad928f06c9037a773914b7cc4b4acedf
c501306014 <c501306014@gmail.com>
2020-11-28 14:38:00 +0900

    update GameDescription.vue ワンナイト人狼とは　の内容を更新

--- vue/src/components/GameDescription.vue
+++ vue/src/components/GameDescription.vue
@@ -1,7 +1,17 @@
 <template>
     <div>
-      <h2>ワンナイト人狼とは？</h2>
-      <p>テキストテキストテキストテキストテキスト</p>
+      <h2>ワンナイト人狼とは</h2>
+      <div class="list-container">
+
+        <ul>
+          <li>
+            ワンナイト人狼は、<a href="https://twitter.com/1nite_jinro">オオカミさん氏</a>作成のカードゲーム人狼を簡単にしたゲームです。<a href="http://1nite-jinro.com/">オフィシャルWEBサイト</a>
+          </li>  
+          <li>
+            <a href="http://commons.nicovideo.jp/material/nc63613">【ワンナイト人狼】ルールブック（公式）</a>
+          </li>
+        </ul>
+      </div>
     </div>
 </template>
 
@@ -11,3 +21,20 @@ export default {
     name:"GameDescription",
 }
 </script>
+
+<style>
+
+.list-container{
+  display: flex;
+  justify-content: center;
+}
+
+
+li{
+  margin: 0, auto;
+  text-align: left;
+  max-width: 60rem;
+}
+
+
+</style>

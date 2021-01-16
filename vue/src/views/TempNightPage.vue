d7a94940fcd49be40c36538d8c5076d5be16beda
c501306014 <c501306014@gmail.com>
2021-01-16 16:36:42 +0900

    リファクタリング

--- vue/src/views/TempNightPage.vue
+++ vue/src/views/TempNightPage.vue
@@ -1,62 +1,64 @@
 <template>
-<div class="night_page">
+  <div class="night_page">
     <div>
-        <span>自分:</span>
-        <span>{{playerName}}</span>
-        <span>({{playerRole.roleName}})</span>
+      <span>自分:</span>
+      <span>{{ playerName }}</span>
+      <span>({{ playerRole.roleName }})</span>
     </div>
-    <hr>
+    <hr />
     <div>
-        他のプレイヤー
-        <ul>
-            <li v-for="player in otherPlayerList" v-bind:key="player.id">
-                <span>{{player.name}}</span>
-                <span>({{player.role.roleName}})</span>
-            </li>
-        </ul>
+      他のプレイヤー
+      <ul>
+        <li v-for="player in otherPlayerList" v-bind:key="player.id">
+          <span>{{ player.name }}</span>
+          <span>({{ player.role.roleName }})</span>
+        </li>
+      </ul>
     </div>
     <div>
-        <a>完了</a>
+      <a>完了</a>
     </div>
-</div>
+  </div>
 </template>
 
 <script>
 import axios from "axios";
 
 export default {
-    name: "TempNightPage",
-    data(){
-        return{
-            playerName: "xxxxx",
-            playerRole: "xxxxx",
-            otherPlayerList: [{
-                id: 1,
-                name: "xxxxx",
-                role: "---"
-            }],
-        }
-    },
-    mounted(){
-        axios.get('http://localhost:8080/night-index',{withCredentials: true})
-        .then((response) => {
-            console.log(response.data);
-            this.playerName = response.data.playerName;
-            this.playerRole = response.data.playerRole;
+  name: "TempNightPage",
+  data() {
+    return {
+      playerName: "xxxxx",
+      playerRole: "xxxxx",
+      otherPlayerList: [
+        {
+          id: 1,
+          name: "xxxxx",
+          role: "---",
+        },
+      ],
+    };
+  },
+  mounted() {
+    axios
+      .get("http://localhost:8080/night-index", { withCredentials: true })
+      .then((response) => {
+        console.log(response.data);
+        this.playerName = response.data.playerName;
+        this.playerRole = response.data.playerRole;
 
-            this.otherPlayerList = response.data.otherPlayerList;
-      }).catch(() => {
-        this.$router.push('/temp-room');
+        this.otherPlayerList = response.data.otherPlayerList;
+      })
+      .catch(() => {
+        this.$router.push("/temp-room");
       });
-    }
-
-}
+  },
+};
 </script>
 
 <style scoped>
 .night_page {
-    text-align: left;
-    margin: 20px;
+  text-align: left;
+  margin: 20px;
 }
-
 </style>

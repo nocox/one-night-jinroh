6a4259e9913e20843f1e4704a8fe2de5586c0e87
c501306014 <c501306014@gmail.com>
2021-01-16 16:35:37 +0900

    add NightPage.vue

---
+++ vue/src/views/NightPage.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="night-page">
+    <h1>夜の行動ページ</h1>
+
+    <div class="player-info me">
+      <h2>自分：{{ playerName }} ({{ playerRole.roleName }})</h2>
+    </div>
+
+    <div class="plyer-info other">
+      <h2>他のプレイヤー</h2>
+      <ul>
+        <li v-for="player in otherPlayerList" v-bind:key="player.id">
+          <span>{{ player.name }}</span>
+          <span>{{ player.role.roleName }}</span>
+        </li>
+      </ul>
+    </div>
+
+    <div>
+      <a>完了</a>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "NightPage",
+  data() {
+    return {
+      playerName: "xxxxx",
+      playerRole: "xxxxx",
+      otherPlayerList: [
+        {
+          id: 1,
+          name: "xxxxx",
+          role: "---",
+        },
+      ],
+    };
+  },
+  mounted() {
+    axios
+      .get("http://localhost:8080/night-index", { withCredentials: true })
+      .then((response) => {
+        console.log(response.data);
+        this.playerName = response.data.playerName;
+        this.playerRole = response.data.playerRole;
+        this.otherPlayerList = response.data.otherPlayerList;
+      })
+      .catch(() => {
+        this.$router.push("/roomtop");
+      });
+  },
+};
+</script>
+
+<style scoped>
+</style>

0717ceda1b22c82775f3f89d9546973d5d416eeb
c501306014 <c501306014@gmail.com>
2021-01-30 15:14:58 +0900

    add RoleCard.vue

---
+++ vue/src/components/RoleCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="card-wrapper">
+    <img src="../assets/card.png" alt="カードの裏側" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "rolecard",
+};
+</script>
+
+<style lang="scss" scoped>
+.card-wrapper {
+  img {
+    height: 150px;
+    width: auto;
+  }
+}
+</style>

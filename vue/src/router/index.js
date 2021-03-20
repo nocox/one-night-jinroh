4437ea8ba9eb68c24eb70e21d7420aac45aa3b3d
c501306014 <c501306014@gmail.com>
2021-03-20 15:39:47 +0900

    vue-routerでページ遷移した時スクロール位置をトップに固定

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -57,7 +57,14 @@ const routes = [
 ]
 
 const router = new VueRouter({
-  routes
+  routes: routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { x: 0, y: 0 }
+    }
+  }
 })
 
 export default router

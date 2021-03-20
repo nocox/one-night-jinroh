62232fa80c3723ebc02305454ca2fa4efe655c38
c501306014 <c501306014@gmail.com>
2021-03-20 15:12:22 +0900

    レスポンシブ機能・スクロールヒント反映

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <router-view/>
+    <router-view />
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
@@ -14,32 +14,60 @@
   </div>
 </template>
 
-<style>
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style lang="scss">
 @font-face {
-    font-family: "Mamelon";
-    src: url("./assets/fonts/Mamelon-3-Hi-Regular.woff2") format("woff2");
-    font-weight: 400;
+  font-family: "Mamelon";
+  src: url("./assets/fonts/Mamelon-3-Hi-Regular.woff2") format("woff2");
+  font-weight: 400;
 }
 
 @font-face {
-    font-family: "Mamelon";
-    src: url("./assets/fonts/Mamelon-5-Hi-Regular.woff2") format("woff2");
-    font-weight: 700;
+  font-family: "Mamelon";
+  src: url("./assets/fonts/Mamelon-5-Hi-Regular.woff2") format("woff2");
+  font-weight: 700;
 }
 
+.pc {
+  display: block;
+}
+.sp {
+  display: none;
+}
 
 #app {
   font-family: Mamelon, Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: left;
+  letter-spacing: 0.05em;
   color: #212121;
 }
 
-*{
+* {
   box-sizing: border-box;
   padding: 0;
   margin: 0;
+}
+
+main {
+  max-width: 1200px;
+  margin: auto;
+  padding: 0 1.5rem;
+}
+
+section {
+  margin: 2rem auto 2rem auto;
+}
+
+h2 {
+  margin: 0.5rem auto 0.5rem auto;
+  font-size: 1.25rem;
 }
 
 #nav {
@@ -55,4 +83,12 @@
   color: #42b983;
 }
 
+@media screen and (max-width: 1024px) {
+  .pc {
+    display: none;
+  }
+  .sp {
+    display: block;
+  }
+}
 </style>

9880b9afbc2356908b02c167e5e7f38e9c4663d6
c501306014 <c501306014@gmail.com>
2021-05-08 15:51:18 +0900

    勝利イメージの表示内容に人狼・吊人を追加

--- vue/src/components/resultImage.vue
+++ vue/src/components/resultImage.vue
@@ -1,25 +1,54 @@
 <template>
-  <section>
-    <img :src="result_image_path[result]" alt="" />
+  <section class="winner">
+    <form action="">
+      <h3>デバッグ用 勝者選択リスト</h3>
+      <label v-for="(val, key) in result_image_path" :key="key">
+        <input type="radio" :value="key" v-model="result" />{{ key }} <br />
+      </label>
+    </form>
+    <div>
+      <img class="winner-image" :src="result_image_path[result]" alt="" />
+    </div>
   </section>
 </template>
 
 <script>
+
+
 export default {
   name: "resultImage",
   props: ["result"],
   data() {
     return {
-      result_image_path:{
-          "平和村成功": require("../assets/images/result1.png")
-      }
+      result_image_path: {
+        平和村成功: require("../assets/images/result1.png"),
+        村人側の勝利: require("../assets/images/result1.png"),
+        人狼側の勝利: require("../assets/images/result2.png"),
+        "人狼側の勝利(潜伏成功)": require("../assets/images/result2.png"),
+        吊人の勝利: require("../assets/images/result3.png"),
+      },
     };
   },
 };
 </script>
 
 <style lang="scss" scoped>
-img{
-    width:20rem;
+div {
+  text-align: center;
+  .winner-image {
+    width: 40rem;
+  }
+}
+
+form {
+  text-align: center;
+}
+
+@media screen and (max-width: 1024px) {
+  div {
+    .winner-image {
+      width: 15rem;
+    }
+  }
 }
 </style>

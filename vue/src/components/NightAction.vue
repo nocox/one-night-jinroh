6baf24458b14f1c18045d9d55022970c335ca307
c501306014 <c501306014@gmail.com>
2021-08-07 14:59:29 +0900

    夜の行動ページのUIを変更した

---
+++ vue/src/components/NightAction.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="night-action-area">
+    <div class="kaito" v-if="roleName == '怪盗'">
+      <p>役職を入れ替える相手を選んでください。</p>
+      <form action="">
+        <ul>
+          <li
+            v-for="otherPlayer in otherPlayerList"
+            v-bind:key="otherPlayer.id"
+          >
+            <label>
+              <input
+                type="radio"
+                v-model="checkedPlayerID"
+                :value="otherPlayer.id"
+                :disabled="is_votable"
+              />
+              {{ otherPlayer.name }}
+            </label>
+          </li>
+        </ul>
+        <myButton :text="'選んだ相手と入れ替える'" :method="''" />
+      </form>
+    </div>
+    <div class="jinroh" v-else-if="roleName == '人狼'">
+      <myButton :text="'仲間を確認する'" :method="''" />
+    </div>
+    <div class="uranai" v-else-if="roleName == '占い師'">
+      <p>占う相手を選んでください。</p>
+      <form action="">
+        <ul>
+          <li>
+            <label
+              ><input type="radio" v-model="checkedPlayerID" :value="0" />
+              場のカード2枚</label
+            >
+          </li>
+          <li
+            v-for="otherPlayer in otherPlayerList"
+            v-bind:key="otherPlayer.id"
+          >
+            <label>
+              <input
+                type="radio"
+                v-model="checkedPlayerID"
+                :value="otherPlayer.id"
+              />
+              {{ otherPlayer.name }}
+            </label>
+          </li>
+        </ul>
+        <myButton :text="'選んだ相手を占う'" :method="''" />
+      </form>
+    </div>
+    <div class="no-action" v-else>村人・狂人・吊人はアクションなし</div>
+  </div>
+</template>
+
+<script>
+import myButton from "@/components/Button.vue";
+// import RoleCard from "@/components/RoleCard.vue";
+
+export default {
+  name: "RoleCardDisplay",
+  components: { myButton },
+  data() {
+    return {
+      checkedPlayerID: 0,
+    };
+  },
+  props: {
+    roleName: {
+      type: Object,
+      required: true,
+    },
+    otherPlayerList: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.night-action-area{
+    text-align: left;
+}
+
+li{
+    list-style: none;
+}
+@media screen and (max-width: 639px) {
+}
+</style>
--- vue/src/components/NightDisplay.vue
+++ vue/src/components/NightDisplay.vue
@@ -1,38 +1,30 @@
 <template>
   <section class="role-card-display-area">
     <div class="player">
-      <h3>あなたです</h3>
       <RoleCard :roleName="playerRole.roleName" :playerName="playerName" />
     </div>
 
-    <div class="not-player">
-      <h3>おやすみ中のカード</h3>
-      <RoleCard
-        v-for="boardCard in boardCards"
-        :key="boardCard.index"
-        :roleName="boardCard.roleName"
-      />
+    <div class="role-description">
+      <div class="role-description__inner">
+        <NightRoleDescription :roleName="playerRole.roleName" />
+      </div>
     </div>
 
-    <div class="other-player">
-      <h3>他のプレイヤー</h3>
-      <RoleCard
-        v-for="player in otherPlayerList"
-        :key="player.id"
-        :roleName="'不明'"
-        :playerName="player.name"
-      />
-      <!-- :roleName="player.role.roleName" -->
+    <div class="action">
+      <NightAction :roleName="playerRole.roleName" :otherPlayerList="otherPlayerList" />
     </div>
+    
   </section>
 </template>
 
 <script>
 import RoleCard from "@/components/RoleCard.vue";
+import NightAction from "@/components/NightAction.vue";
+import NightRoleDescription from "@/components/NightRoleDescription";
 
 export default {
   name: "RoleCardDisplay",
-  components: { RoleCard },
+  components: { RoleCard, NightAction, NightRoleDescription },
   props: {
     playerRole: {
       type: Object,
@@ -69,72 +61,53 @@ export default {
 .role-card-display-area {
   justify-content: center;
   text-align: center;
-  display: flex;
-  flex-wrap: wrap;
+  display: grid;
+  grid-template-columns: 1fr 2fr;
 }
 
 .player {
   display: flex;
-  width: 33%;
   justify-content: center;
-  flex-wrap: wrap;
+  align-items: center;
+  flex-direction: column;
+  background-color: #eee;  
+  padding: 1em;
+}
 
-  h3 {
-    width: 100%;
-  }
-  .card-wrapper {
-    padding-top: 16px;
-    width: 100%;
+.role-description{
+background-color: #eee;  
+padding: 24px;
+  .role-description__inner{
+    background-color: #fff;
+    padding: 1em;
+    text-align: left;
+    border-radius: 8px;
+    box-shadow: 5px 5px 5px rgba(0,0,0,.1);
+    
   }
 }
 
-.not-player {
-  width: 66%;
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-evenly;
-  h3 {
-    width: 100%;
-  }
-  .card-wrapper {
-    padding-top: 16px;
-    width: 50%;
-  }
+
+.action{
+  margin-top: 16px;
+  padding: 1em;
+  border: 3px solid #eee;
+grid-column: 1/3;  
+height  :20rem;
+  border-radius: 8px;
 }
 
-.other-player {
-  padding-top: 24px;
-  width: 100%;
-  display: flex;
-  flex-wrap: wrap;
-
-  h3 {
-  text-align: left;  
-    width: 100%;
-  }
-  .card-wrapper {
-    padding-top: 16px;
-  }
-}
 
 @media screen and (max-width: 639px) {
+  .role-card-display-area {
+    grid-template-columns: 1fr;
+  }
+  .action{
+    grid-column: 1/2;
+  }
   h3 {
     font-size: 16px;
   }
 
-.other-player {
-  
-  
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
-
-  h3 {
-    width: 100%;
-    grid-column: 1/4;
-  }
-  .card-wrapper {
-    padding-top: 16px;
-  }
-}
 }
 </style>

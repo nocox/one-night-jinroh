bab87d06b3b6d42089f9f0f7f634f046153b169f
c501306014 <c501306014@gmail.com>
2021-09-18 14:51:10 +0900

    COボタンブロックをレスポンシブ対応した。

--- vue/src/components/CoButtonArea.vue
+++ vue/src/components/CoButtonArea.vue
@@ -2,16 +2,10 @@
   <section class="co-button-area">
     <h2>COボタン</h2>
     <div class="co-icons">
-      <a
-        :class="{ current: isActive['村人'] }"
-        @click="toggleIcon('村人')"
-      >
+      <a :class="{ current: isActive['村人'] }" @click="toggleIcon('村人')">
         <img src="../assets/images/chara-icon/murabito.png" alt="" />
       </a>
-      <a
-        :class="{ current: isActive['占い師'] }"
-        @click="toggleIcon('占い師')"
-      >
+      <a :class="{ current: isActive['占い師'] }" @click="toggleIcon('占い師')">
         <img src="../assets/images/chara-icon/uranaishi.png" alt="" />
       </a>
       <a :class="{ current: isActive['怪盗'] }" @click="toggleIcon('怪盗')">
@@ -23,10 +17,7 @@
       <a :class="{ current: isActive['狂人'] }" @click="toggleIcon('狂人')">
         <img src="../assets/images/chara-icon/kyojin.png" alt="" />
       </a>
-      <a
-        :class="{ current: isActive['吊り人'] }"
-        @click="toggleIcon('吊り人')"
-      >
+      <a :class="{ current: isActive['吊り人'] }" @click="toggleIcon('吊り人')">
         <img src="../assets/images/chara-icon/tsuribito.png" alt="" />
       </a>
     </div>
@@ -53,16 +44,12 @@ export default {
         this.isActive[key] = false;
       });
       this.isActive[roleName] = true;
-      this.$emit('getActive', roleName);
+      this.$emit("getActive", roleName);
     },
   },
-  mounted(){
+  mounted() {
     let nowRole = "村人";
-    // for(let role in this.isActive){
-    //   console.log(this.isActive[role]);
-    // }
-    this.$emit('getActive', nowRole);
-    
+    this.$emit("getActive", nowRole);
   },
 };
 </script>
@@ -83,7 +70,7 @@ export default {
     display: flex;
     flex-wrap: wrap;
     justify-content: center;
-    width: 100%;
+    width: 66%;
 
     a {
       width: 33%;
@@ -93,7 +80,8 @@ export default {
     }
 
     a img {
-      // filter:grayscale(100%);
+      width: 100%;
+      height: auto;
       border: 5px solid transparent;
       border-radius: 50%;
       opacity: 0.5;
@@ -102,8 +90,11 @@ export default {
     .current img {
       border: 5px solid lighten(lightgreen, 0);
       box-shadow: 0 0 10px rgba(128, 128, 128, 0.5);
-      opacity: 1; // filter:grayscale(0%);
+      opacity: 1;
     }
   }
+}
+
+@media screen and (max-width: 639px) {
 }
 </style>

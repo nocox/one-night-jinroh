447ffaddc5a3caad69e50dc8ee7e8461a276a156
c501306014 <c501306014@gmail.com>
2022-02-26 16:06:11 +0900

    lint適用

--- spring/src/main/java/com/okaka/onenightjinroh/application/service/result/JudgeFacade.java
+++ spring/src/main/java/com/okaka/onenightjinroh/application/service/result/JudgeFacade.java
@@ -21,34 +21,34 @@ import java.util.stream.Collectors;
 
 @Service
 public class JudgeFacade {
-    @Autowired
-    TallyResultPort tallyResultPort;
-    @Autowired
-    KaitoNightActRepository kaitoNightActRepository;
-    @Autowired
-    KaitoNightActExecuteLogic kaitoNightActExecuteLogic;
+        @Autowired
+        TallyResultPort tallyResultPort;
+        @Autowired
+        KaitoNightActRepository kaitoNightActRepository;
+        @Autowired
+        KaitoNightActExecuteLogic kaitoNightActExecuteLogic;
 
-    List<WinLoseConditionBase> winLoseConditions = Arrays.asList(
-            new SimpleVillageWin(),
-            new SimpleJinrohWin(),
-            new SuccessPeaceVillage(),
-            new FailPeaceVillage(),
-            new SuccessHideJinrohWin()
-    );
+        List<WinLoseConditionBase> winLoseConditions = Arrays.asList(
+                        new SimpleVillageWin(),
+                        new SimpleJinrohWin(),
+                        new SuccessPeaceVillage(),
+                        new FailPeaceVillage(),
+                        new SuccessHideJinrohWin());
 
-    public Judge judge(Long gameId) {
-        KaitoNightActFormatter kaitoNightActFormatter = kaitoNightActRepository.findByGameId(gameId)
-                .map(kaitoNightAct -> kaitoNightActExecuteLogic.invoke(kaitoNightAct))
-                .orElse(null);
+        public Judge judge(Long gameId) {
+                KaitoNightActFormatter kaitoNightActFormatter = kaitoNightActRepository.findByGameId(gameId)
+                                .map(kaitoNightAct -> kaitoNightActExecuteLogic.invoke(kaitoNightAct))
+                                .orElse(null);
 
-        List<TallyResultConsideredNightAct> tallyResults = tallyResultPort.searchTallyResults(gameId).stream()
-                .map(tallyResult -> new TallyResultConsideredNightAct(tallyResult, kaitoNightActFormatter))
-                .collect(Collectors.toList());
+                List<TallyResultConsideredNightAct> tallyResults = tallyResultPort.searchTallyResults(gameId).stream()
+                                .map(tallyResult -> new TallyResultConsideredNightAct(tallyResult,
+                                                kaitoNightActFormatter))
+                                .collect(Collectors.toList());
 
-        WinLoseConditionBase winLoseConditionBase = winLoseConditions.stream()
-                .filter(conditionBase -> conditionBase.condition(tallyResults))
-                .min(Comparator.comparing(WinLoseConditionBase::priority))
-                .orElseThrow();
-        return new Judge(winLoseConditionBase.getResultText());
-    }
+                WinLoseConditionBase winLoseConditionBase = winLoseConditions.stream()
+                                .filter(conditionBase -> conditionBase.condition(tallyResults))
+                                .min(Comparator.comparing(WinLoseConditionBase::priority))
+                                .orElseThrow();
+                return new Judge(winLoseConditionBase.getResultText());
+        }
 }

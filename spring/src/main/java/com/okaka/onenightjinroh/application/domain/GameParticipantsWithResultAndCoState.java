18e6cb707bb03836109249f2e4c71b7d879d5d26
c501306014 <c501306014@gmail.com>
2022-03-26 16:51:39 +0900

    GameParticipantsWithResultAndCoState クラスを実装

---
+++ spring/src/main/java/com/okaka/onenightjinroh/application/domain/GameParticipantsWithResultAndCoState.java
@@ -0,0 +1,63 @@
+package com.okaka.onenightjinroh.application.domain;
+
+import com.okaka.onenightjinroh.application.service.result.WinLoseConditionBase;
+
+import java.util.Optional;
+import java.util.stream.Stream;
+
+public class GameParticipantsWithResultAndCoState {
+    private final GameParticipantsConsideredNightAct participants;
+    private final WinLoseConditionBase winLoseCondition;
+    private final CoState coState;
+
+    public GameParticipantsWithResultAndCoState(GameParticipantsConsideredNightAct participants, WinLoseConditionBase winLoseCondition, CoState coState) {
+        this.participants = participants;
+        this.winLoseCondition = winLoseCondition;
+        this.coState = coState;
+    }
+
+    public Stream<GameParticipantWithResultAndCoState> stream() {
+        return participants.stream().map(GameParticipantWithResultAndCoState::new);
+    }
+
+    public class GameParticipantWithResultAndCoState{
+        private final GameParticipantsConsideredNightAct.GameParticipantConsideredNightAct participant;
+
+        public GameParticipantWithResultAndCoState(GameParticipantsConsideredNightAct.GameParticipantConsideredNightAct participant) {
+            this.participant = participant;
+        }
+
+        public String getPlayerName() {
+            return participant.getParticipant().getUser().getUserName();
+        }
+
+        public String getOriginalRoleEngStr() {
+            return participant.getParticipant().getRole().getRoleEngName();
+        }
+
+        public String getRoleEngStrConsideredNightAct() {
+            return participant.getRoleConsideredNightAct().getRoleEngName();
+        }
+
+        public String getCoRoleEngStr() {
+            Optional<CoState.Co> co = coState.getCos().stream()
+            .filter(c-> c.getId() == participant.getParticipant().getGameParticipationId())
+            .findFirst();
+            
+            return co.map(c-> c.getRole().getRoleEngName()).orElse("");
+        }
+
+        public String getWinOrLose() {
+            Role roleConsideredNightAct = participant.getRoleConsideredNightAct();
+            return winLoseCondition.judge(roleConsideredNightAct).name();
+        }
+
+        public boolean myself(Long participantId) {
+            return participant.getParticipant().getGameParticipationId().equals(participantId);
+        }
+
+        public String getComment() {
+            return "mada dayo";
+        }
+    }
+}

ad2746a5022632dd164e33288aa878c56c31481f
nocox <ddnd7nocox@gmail.com>
2022-05-06 02:59:24 +0900

    注意: npm serverができなくなる. springでhosting

--- spring/src/main/java/com/okaka/onenightjinroh/WebSocketConfig.java
+++ spring/src/main/java/com/okaka/onenightjinroh/WebSocketConfig.java
@@ -22,7 +22,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/jinroh-websocket")
-                .setAllowedOrigins(myServiceBean.getDomain())
                 .withSockJS();
     }
 }

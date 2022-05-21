8afac349c7a3016396baf5bafb49ec8192301999
nocox <ddnd7nocox@gmail.com>
2022-05-21 14:32:51 +0900

    Revert "不要になった記述を削除した"
    This reverts commit 730f5b74088720f8d492811bfb2f576ee3e6e2cc.

---
+++ spring/src/main/java/com/okaka/onenightjinroh/MyServiceBean.java
@@ -0,0 +1,15 @@
+package com.okaka.onenightjinroh;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyServiceBean {
+
+    @Value("${com.okaka.onenightjinroh.domain}")
+    private String domain;
+
+    public String getDomain() {
+        return domain;
+    }
+}
--- spring/src/main/java/com/okaka/onenightjinroh/WebSocketConfig.java
+++ spring/src/main/java/com/okaka/onenightjinroh/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package com.okaka.onenightjinroh;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -9,6 +10,8 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Autowired
+    MyServiceBean myServiceBean;
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {

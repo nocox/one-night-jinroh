9afd1e08456ed3c150167cba49800a2196129084
c501306014 <c501306014@gmail.com>
2022-06-04 16:49:14 +0900

    デフォルトプレイヤー名を変更した

--- spring/src/main/java/com/okaka/onenightjinroh/application/service/top/JoinedRoomUseCase.java
+++ spring/src/main/java/com/okaka/onenightjinroh/application/service/top/JoinedRoomUseCase.java
@@ -1,8 +1,13 @@
 package com.okaka.onenightjinroh.application.service.top;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import com.okaka.jinroh.persistence.RoomParticipant;
 import com.okaka.jinroh.persistence.RoomParticipantDao;
 import com.okaka.jinroh.persistence.UserEntity;
+import com.okaka.onenightjinroh.application.repository.UserRepository;
 import com.okaka.jinroh.persistence.UserDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,10 +20,12 @@ public class JoinedRoomUseCase {
     @Autowired
     RoomParticipantDao roomParticipantDao;
 
+    @Autowired
+    UserRepository userRepository;
+
     public JoinedRoomUseCaseDto joinedRoom(Long roomId) {
-        int userCount = userDao.selectCount();
         UserEntity userEntity = new UserEntity();
-        userEntity.user_name = "プレイヤー" + (userCount + 1);
+        userEntity.user_name = getUseablePlayerNameOptions(roomId).get(0);
         userDao.insert(userEntity);
 
         RoomParticipant roomParticipant = new RoomParticipant();
@@ -28,5 +35,32 @@ public class JoinedRoomUseCase {
         roomParticipantDao.insert(roomParticipant);
 
         return new JoinedRoomUseCaseDto(userEntity);
+    }
+
+    private List<String> PLAYER_NAME_OPTIONS = Arrays.asList(
+        "いぬ",
+        "ねこ",
+        "ひよこ",
+        "ぺんぎん",
+        "ねずみ",
+        "さる",
+        "ぱんだ",
+        "くま",
+        "あるぱか"
+    );
+    
+    private List<String> getUsedPlayerNames(Long roomId) {
+        return userRepository.findUsersByRoomId(roomId).stream()
+        .map( user -> user.getUserName())
+        .collect(Collectors.toList());
+    }
+    
+    private List<String> getUseablePlayerNameOptions(Long roomId) {
+        return PLAYER_NAME_OPTIONS.stream()
+        .filter(option -> {
+            return getUsedPlayerNames(roomId).stream()
+            .noneMatch(name -> name == option);
+        })
+        .collect(Collectors.toList());
     }
 }

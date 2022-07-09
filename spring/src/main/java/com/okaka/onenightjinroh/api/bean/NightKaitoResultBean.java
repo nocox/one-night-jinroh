6865b799d6a167fb491705dc64e14062cb6a73e5
nocox <ddnd7nocox@gmail.com>
2022-07-09 16:06:53 +0900

    怪盗の夜の行動をリロードしても再行動できないようにした

--- spring/src/main/java/com/okaka/onenightjinroh/api/bean/NightKaitoResultBean.java
+++ spring/src/main/java/com/okaka/onenightjinroh/api/bean/NightKaitoResultBean.java
@@ -3,13 +3,19 @@ package com.okaka.onenightjinroh.api.bean;
 import com.okaka.onenightjinroh.application.domain.KaitoNightActFormatter;
 
 public class NightKaitoResultBean {
+    private final Long selectedParticipantId;
     private final String actLog;
 
     public NightKaitoResultBean(KaitoNightActFormatter formatter) {
+        this.selectedParticipantId = formatter.getToParticipant().getGameParticipationId();
         this.actLog = formatter.toActLog();
     }
 
     public String getActLog() {
         return actLog;
+    }
+
+    public Long getSelectedParticipantId() {
+        return selectedParticipantId;
     }
 }

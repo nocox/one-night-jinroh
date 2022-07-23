3effbfeaffd53d0e4ab5df4b092880b5f79eee87
c501306014 <c501306014@gmail.com>
2022-07-23 16:46:23 +0900

    RoleImageとRoleDescriptionをROLE_INFOにまとめた

--- vue/src/components/NightRoleDescription.vue
+++ vue/src/components/NightRoleDescription.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="role-description__inner">
     <figure class="card-wrapper">
-      <img :src="RoleImage[roleName]" :alt="RoleImage[roleName]" />
+      <img :src="getRole.img" :alt="roleName" />
       <figcaption v-if="roleName != '不明'">{{ roleName }}</figcaption>
     </figure>
     <div class="description">
       <p>あなたの役職は『{{ roleName }}』です。</p>
       <section>
         <p>夜の行動</p>
-        <p>{{ RoleDescription[roleName] }}</p>
+        <p>{{ getRole.desc }}</p>
       </section>
     </div>
   </div>
@@ -17,54 +17,61 @@
 <script>
 export default {
   name: "NightRoleDescription",
-  components: {},
   props: {
     roleName: {
       type: String,
       required: true,
     },
   },
-  data() {
-    return {
-      RoleCard: {
-        id: 0,
-        name: "---",
-        img: "---",
-      },
-      RoleImage: {
-        不明: require("../assets/images/card.png"),
-        人狼: require("../assets/images/chara/chara1.png"),
-        村人: require("../assets/images/chara/chara2.png"),
-        占い師: require("../assets/images/chara/chara3.png"),
-        怪盗: require("../assets/images/chara/chara4.png"),
-        狂人: require("../assets/images/chara/chara5.png"),
-        吊り人: require("../assets/images/chara/chara6.png"),
-      },
-      RoleDescription: {
-        人狼: `他のプレイヤーに人狼がいるかどうか確認できます。`,
-        村人: `夜の行動はありません。
-                OKボタンを押して夜が明けるのを待ちましょう。`,
-        占い師: `『他のプレイヤー』ひとりまたは『おやすみ中のカード』の役職を確認できます。`,
-        怪盗: `『他のプレイヤー』ひとりと役職を交換できます。
-                勝利条件は交換後の役職に準じます。`,
-        狂人: `夜の行動はありません。
-                OKボタンを押して夜が明けるのを待ちましょう。`,
-        吊り人: `夜の行動はありません。
-                OKボタンを押して夜が明けるのを待ちましょう。`,
-      },
-    };
+  computed: {
+    getRole() {
+      return ROLE_INFO[this.roleName]
+    }
+  },
+};
+
+// FIX ME：KEYを日本語から英語に直す
+// FIX ME：ROLE_INFOの定義をJSファイルに切り出して、ライブラリとして参照できるようにする
+const ROLE_INFO = {
+  不明: { img: require("../assets/images/card.png"), desc: "不明" },
+  人狼: {
+    img: require("../assets/images/chara/chara1.png"),
+    desc: `他のプレイヤーに人狼がいるかどうか確認できます。`,
+  },
+  村人: {
+    img: require("../assets/images/chara/chara2.png"),
+    desc: `夜の行動はありません。`,
+  },
+  占い師: {
+    img: require("../assets/images/chara/chara3.png"),
+    desc: `『他のプレイヤー』ひとりまたは『おやすみ中のカード』の役職を確認できます。`,
+  },
+  怪盗: {
+    img: require("../assets/images/chara/chara4.png"),
+    desc: `『他のプレイヤー』ひとりと役職を交換できます。
+            勝利条件は交換後の役職に準じます。`,
+  },
+  狂人: {
+    img: require("../assets/images/chara/chara5.png"),
+    desc: `夜の行動はありません。
+            OKボタンを押して夜が明けるのを待ちましょう。`,
+  },
+  吊り人: {
+    img: require("../assets/images/chara/chara6.png"),
+    desc: `夜の行動はありません。
+            OKボタンを押して夜が明けるのを待ちましょう。`,
   },
 };
 </script>
 
 <style lang="scss" scoped>
-.role-description__inner{
+.role-description__inner {
   display: grid;
   grid-template-columns: 1fr 2fr;
   justify-items: center;
 }
 
-.description{
+.description {
   width: 100%;
   padding: 1em;
   text-align: left;
@@ -73,9 +80,8 @@ export default {
   box-shadow: 5px 5px 5px rgb(0 0 0 / 10%);
 }
 
-.card-wrapper{
+.card-wrapper {
   max-width: 160px;
-  
 }
 
 img {

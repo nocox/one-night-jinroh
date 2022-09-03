451d589524653682034c89b7135cdcddb67de206
nocox <ddnd7nocox@gmail.com>
2022-09-03 16:02:30 +0900

    SelectablePlayerの役割を全部TallyPlayerAreaに委譲した

--- vue/src/components/TallyPlayerArea.vue
+++ vue/src/components/TallyPlayerArea.vue
@@ -2,43 +2,48 @@
   <section class="co-area">
     <div class="player-wrapper">
       <!-- 自分 -->
-      <SelectablePlayer
-        :playerName="player.playerName"
-        :roleName="player.playerRole.roleName"
-        :coRole="coMap(player.playerId)"
-        :selectedPlayers="selectedPlayers"
-        :myself="true"
+      <Player
+          :playerName="player.playerName"
+          :roleName="player.playerRole.roleName"
+          :coRole="coMap(player.playerId)"
+          :selectedPlayers="selectedPlayers"
+          :myself="true"
+          :isSelected=isSelected(player.playerName)
       />
       <!-- 他のプレイヤー -->
-      <SelectablePlayer
-        :playerName="val.name"
-        :roleName="val.role.roleName"
-        :coRole="coMap(val.id)"
-        :selectedPlayers="selectedPlayers"
-        v-for="(val, key) in otherPlayerList"
-        :myself="false"
-        :key="key"
+      <Player
+          v-for="(val, key) in otherPlayerList"
+          :key="key"
+          :playerName="val.name"
+          :roleName="val.role.roleName"
+          :coRole="coMap(val.id)"
+          :selectedPlayers="selectedPlayers"
+          :myself="false"
+          :isSelected=isSelected(val.name)
       />
     </div>
   </section>
 </template>
 
 <script>
-import SelectablePlayer from "@/components/SelectablePlayer.vue";
+import Player from "@/components/Player";
 
 export default {
   name: "TallyPlayerArea",
-  props: ["otherPlayerList", "player", "cos", "selectedPlayers"],
-  components: { SelectablePlayer },
+  props: ["otherPlayerList", "player", "cos", "selectedPlayers"],// TODO: selectedPlayersに必須をつけたい
+  components: {Player},
   methods: {
     coMap: function (playerId) {
-      const roleName = this.cos.find(co => co.id == playerId)
+      const roleName = this.cos.find(co => co.id === playerId)
 
-      if(roleName){
+      if (roleName) {
         return roleName.role;
-      }else{
+      } else {
         return "murabito";
       }
+    },
+    isSelected: function (name) {
+      return this.selectedPlayers.map(it => it.name).includes(name);
     },
   },
 };
@@ -51,7 +56,7 @@ export default {
 
   .player-wrapper {
     display: flex;
-    flex-wrap:wrap;
+    flex-wrap: wrap;
     row-gap: 1rem;
     justify-content: space-between;
     padding: 0 1rem;

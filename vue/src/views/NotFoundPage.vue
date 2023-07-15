ed03e30cd8683c84d732ccd175a920e905e20610
c501306014 <c501306014@gmail.com>
2023-07-15 15:02:58 +0900

    404 NotFoundPage を実装した

---
+++ vue/src/views/NotFoundPage.vue
@@ -0,0 +1,35 @@
+<template>
+  <main class="not-found-page">
+    <h1>ページが見つかりません</h1>
+    <myButton :text="'戻る'" :method="toPrev" />
+  </main>
+</template>
+
+<script>
+import myButton from "@/components/Button.vue";
+export default {
+  name: "NotFoundPage",
+  data() {
+    return {};
+  },
+  components: {
+    myButton,
+  },
+  mounted() {},
+  methods: {
+    toPrev: function () {
+      this.$router.go(-1);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.not-found-page {
+  display: grid;
+  grid-template-columns: 1fr;
+  justify-content: center;
+  justify-items: center;
+  padding: 1rem;
+}
+</style>

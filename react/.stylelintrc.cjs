8781350ce0ec340b3b8e8d1192a4ab431e200ae9
c501306014 <c501306014@gmail.com>
2023-07-16 15:29:20 +0900

    panda css を導入した

--- react/.stylelintrc.cjs
+++ react/.stylelintrc.cjs
@@ -1,5 +1,5 @@
 module.exports = {
   extends: ['stylelint-config-standard', 'stylelint-config-recess-order'],
-  ignoreFiles: ['**/node_modules/**'],
+  ignoreFiles: ['**/node_modules/**', '**/styled-system/**', '**/dist/**'],
   plugins: ['stylelint-order'],
 };

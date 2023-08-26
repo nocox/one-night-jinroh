a5fb4afac27a7146e20808ad304b9a1858244e81
c501306014 <c501306014@gmail.com>
2023-08-26 21:30:56 +0900

    ルーム参加処理用の型定義を作成

---
+++ react/src/features/top/type.ts
@@ -0,0 +1,21 @@
+import { z } from 'zod';
+
+type JoinRoomDto = {
+  roomId: string;
+};
+
+type JoinedRoomStatus = 'JOIN_SUCCESS' | 'ROOM_NOT_EXIST' | 'PARTICPANT_LIMIT';
+
+const joinedRoomStatusSchema = z.union([
+  z.literal('JOIN_SUCCESS'),
+  z.literal('ROOM_NOT_EXIST'),
+  z.literal('PARTICPANT_LIMIT'),
+]);
+
+export const isJoinedRoomStatus = (
+  value: unknown,
+): value is JoinedRoomStatus => {
+  return joinedRoomStatusSchema.safeParse(value).success;
+};
+
+export type JoinRoom = (dto: JoinRoomDto) => Promise<JoinedRoomStatus>;

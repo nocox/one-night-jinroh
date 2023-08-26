20bd5e2c4de641ee1a97fc7c677e9a6183b08b89
c501306014 <c501306014@gmail.com>
2023-08-26 23:28:00 +0900

    InvalidResponseBodyError を実装した

--- react/src/error.ts
+++ react/src/error.ts
@@ -1,3 +1,11 @@
+/**
+ * @fileoverview Custom error classes.
+ */
+
+/**
+ * @description 網羅性チェック用の例外クラス
+ * @see https://typescriptbook.jp/reference/statements/never
+ */
 export class ExhaustiveError extends Error {
   // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
   constructor(value: never, message = `Unsupported type: ${value}`) {
@@ -6,5 +14,14 @@ export class ExhaustiveError extends Error {
 
   static {
     this.prototype.name = 'ExhaustiveError';
+  }
+}
+
+/**
+ * @description APIのレスポンスが不正な場合に投げられる例外クラス
+ */
+export class InvalidResponseBodyError extends Error {
+  static {
+    this.prototype.name = 'InvalidResponseBodyError';
   }
 }

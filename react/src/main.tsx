2a753847229ac7e567cf809b628debf23e002a6a
nocox <ddnd7nocox@gmail.com>
2023-09-09 13:14:07 +0900

    approuterの場所を変えるので一旦コメントアウト

--- react/src/main.tsx
+++ react/src/main.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import AppRouter from './routes/AppRouter';
+// import AppRouter from './routes/AppRouter';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <AppRouter />
-  </React.StrictMode>,
+  <React.StrictMode/>,
 );
--- react/src/routes/AppRouter.tsx
+++ react/src/routes/AppRouter.tsx
@@ -1,14 +1,14 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { RoomPage } from '@/features/room/RoomPage';
-import { TopPage } from '@/features/top/TopPage';
-
-const AppRouter: React.FC = () => (
-  <Router>
-    <Routes>
-      <Route path="/" element={<TopPage />} />
-      <Route path="/room" element={<RoomPage />} />
-    </Routes>
-  </Router>
-);
-
-export default AppRouter;
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import { RoomPage } from '@/features/room/RoomPage';
+// import { TopPage } from '@/features/top/TopPage';
+//
+// // const AppRouter: React.FC = () => (
+// //   <Router>
+// //     <Routes>
+// //       <Route path="/" element={<TopPage />} />
+// //       <Route path="/room" element={<RoomPage />} />
+// //     </Routes>
+// //   </Router>
+// // );
+// //
+// // export default AppRouter;

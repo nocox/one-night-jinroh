4165314772c63c2f26acba25b12352980adbb5a2
c501306014 <c501306014@gmail.com>
2023-09-16 19:32:37 +0900

    AppRouterに夜の行動ページを追加

--- react/src/routes/AppRouter.tsx
+++ react/src/routes/AppRouter.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { NightPage } from '@/features/game/night/NightPage';
 import { RoomPage } from '@/features/room/RoomPage';
 import { TopPage } from '@/features/top/TopPage';
 
@@ -7,6 +8,7 @@ const AppRouter: React.FC = () => (
     <Routes>
       <Route path="/" element={<TopPage />} />
       <Route path="/room" element={<RoomPage />} />
+      <Route path="/night" element={<NightPage />} />
     </Routes>
   </Router>
 );

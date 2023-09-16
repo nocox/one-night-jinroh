02ec38929c149367dc479d42a8294de6a590e089
c501306014 <c501306014@gmail.com>
2023-09-16 16:00:20 +0900

    Revert "WebSocket関連のパス定義をapi.tsに追加"
    This reverts commit 386595e314df99c31da880d1965723c2edce3cd5.

--- react/src/features/room/hooks/useWebSocket.ts
+++ react/src/features/room/hooks/useWebSocket.ts
@@ -3,39 +3,36 @@ import SockJS from 'sockjs-client/dist/sockjs';
 import Stomp from 'webstomp-client';
 import type { GameInfo } from '../type';
 import { isGameInfo } from '../type';
-import { getSubscribePath, webSocketPath } from '../url';
 import { InvalidResponseBodyError } from '@/error';
+import { JINROH_API_BASE_URL } from '@/url';
 
 export const useWebSocket = (uuid: string, hostFlg: boolean): void => {
   const [gameInfo, setGameInfo] = useState<GameInfo | undefined>(undefined);
   console.log(gameInfo);
 
   useEffect(() => {
-    const socket = new SockJS(webSocketPath);
+    const socket = new SockJS(JINROH_API_BASE_URL + '/jinroh-websocket');
     const stompClient = Stomp.over(socket);
 
     stompClient.connect({}, () => {
-      stompClient.subscribe(
-        getSubscribePath('receiveGameStart', uuid),
-        (message) => {
-          const gameInfo = message.body
-            ? (JSON.parse(message.body) as unknown)
-            : undefined;
+      stompClient.subscribe(`/topic/${uuid}`, (message) => {
+        const gameInfo = message.body
+          ? (JSON.parse(message.body) as unknown)
+          : undefined;
 
-          if (!isGameInfo(gameInfo)) {
-            throw new InvalidResponseBodyError(
-              `Invalid response body: ${JSON.stringify(gameInfo)}`,
-            );
-          }
+        if (!isGameInfo(gameInfo)) {
+          throw new InvalidResponseBodyError(
+            `Invalid response body: ${JSON.stringify(gameInfo)}`,
+          );
+        }
 
-          gameInfo.roleList.sort((a, b) => a.roleId - b.roleId);
-          setGameInfo(gameInfo);
+        gameInfo.roleList.sort((a, b) => a.roleId - b.roleId);
+        setGameInfo(gameInfo);
 
-          // TODO: 夜の行動ページを実装し、そこにリダイレクトさせる処理を実装する
-        },
-      );
+        // TODO: 夜の行動ページを実装し、そこにリダイレクトさせる処理を実装する
+      });
 
-      stompClient.subscribe(getSubscribePath('receiveFinishRoom', uuid), () => {
+      stompClient.subscribe('/topic/receive-finish-room/' + uuid, () => {
         if (!hostFlg) {
           window.alert('ホストがルームを解散しました');
         }
--- react/src/features/room/url.ts
+++ react/src/features/room/url.ts
@@ -1,5 +1,5 @@
-import { ExhaustiveError } from '@/error';
-import { JINROH_API_BASE_URL } from '@/url';
+import { ExhaustiveError } from "@/error";
+import { JINROH_API_BASE_URL } from "@/url";
 
 type BaseUrl = typeof JINROH_API_BASE_URL;
 
@@ -7,56 +7,27 @@ type BackendApiPaths = {
   getRoomIndex: '/room-index';
   getGameStart: '/game-start';
   getRoomFinish: '/room-finish';
-};
+}
 
 const backendApiPaths: BackendApiPaths = {
   getRoomIndex: '/room-index',
   getGameStart: '/game-start',
   getRoomFinish: '/room-finish',
-};
+}
 
 type BackendApiPathName = keyof BackendApiPaths;
 type BackendApiPath = (typeof backendApiPaths)[keyof typeof backendApiPaths];
 
-export const getBackendApiPath = (
-  path: BackendApiPathName,
-): `${BaseUrl}${BackendApiPath}` => {
+export const getBackendApiPath = (path: BackendApiPathName): `${BaseUrl}${BackendApiPath}` => {
   switch (path) {
     case 'getRoomIndex':
     case 'getGameStart':
-    case 'getRoomFinish': {
-      return `${JINROH_API_BASE_URL}${backendApiPaths[path]}`;
-    }
+    case 'getRoomFinish':
+      {
+        return `${JINROH_API_BASE_URL}${backendApiPaths[path]}`;
+      }
     default:
       throw new ExhaustiveError(path);
   }
-};
+}
 
-type WebSocketPath = `${BaseUrl}/jinroh-websocket`;
-export const webSocketPath: WebSocketPath = `${JINROH_API_BASE_URL}/jinroh-websocket`;
-
-type SubscribePaths = {
-  receiveGameStart: (uuid: string) => `/topic/${string}`;
-  receiveFinishRoom: (uuid: string) => `/topic/${string}`;
-};
-
-const subscribePaths: SubscribePaths = {
-  receiveGameStart: (uuid) => `/topic/${uuid}`,
-  receiveFinishRoom: (uuid) => `/topic/receive-finish-room/${uuid}`,
-};
-
-type SubscribePathName = keyof SubscribePaths;
-
-export const getSubscribePath = (
-  path: SubscribePathName,
-  uuid: string,
-): string => {
-  switch (path) {
-    case 'receiveGameStart':
-    case 'receiveFinishRoom': {
-      return `${subscribePaths[path](uuid)}`;
-    }
-    default:
-      throw new ExhaustiveError(path);
-  }
-};

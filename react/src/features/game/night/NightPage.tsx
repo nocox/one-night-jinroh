f95f30313d145c340292e7db246aafcad846dfdd
c501306014 <c501306014@gmail.com>
2023-09-25 20:27:13 +0900

    WIP: night pageを実装

--- react/src/features/game/night/NightPage.tsx
+++ react/src/features/game/night/NightPage.tsx
@@ -1,16 +1,15 @@
 import { NightTemplate } from './NightTemplate';
 import { useNightData } from './hooks/useNightData';
+import { Loading } from '@/components';
 
 export const NightPage: React.FC = () => {
-  const { doneNightAct, gameId, gameIndex } = useNightData();
+  const nightIndexResponseBody = useNightData();
 
-  return (
+  return nightIndexResponseBody === undefined ? (
+    <Loading />
+  ) : (
     <>
-      <NightTemplate
-        doneNightAct={doneNightAct}
-        gameId={gameId}
-        gameIndex={gameIndex}
-      />
+      <NightTemplate nightIndexResponseBody={nightIndexResponseBody} />
     </>
   );
 };
--- react/src/features/game/night/NightTemplate.tsx
+++ react/src/features/game/night/NightTemplate.tsx
@@ -1,32 +1,41 @@
+import { css } from 'styled-system/css';
+import { NightActionForm } from './components/NightActionForm';
+import { RoleDescription } from './components/RoleDescription';
+import type { NightIndexResponseBody } from './type';
 import { DefaultLayout } from '@/components';
-import type { GameIndex } from '@/features/game/type';
 
-type Props = {
-  doneNightAct: boolean;
-  gameId: number;
-  gameIndex: GameIndex;
+const styles = {
+  title: css({
+    fontWeight: 'bold',
+    fontSize: '1.25rem',
+    textAlign: 'center',
+    margin: '1rem 0',
+  }),
+  roleDescription: css({}),
 };
 
-export const NightTemplate: React.FC<Props> = ({
-  doneNightAct,
-  gameId,
-  gameIndex,
-}) => {
-  console.log('gameIndex', gameIndex);
-  console.log('doneNightAct', doneNightAct);
-  console.log('gameId', gameId);
-  // TODO テンプレートの実装を進める
+type Props = {
+  nightIndexResponseBody: NightIndexResponseBody;
+};
+
+export const NightTemplate: React.FC<Props> = ({ nightIndexResponseBody }) => {
+  const { gameIndex, doneNightAct, gameId } = nightIndexResponseBody;
+  console.log(gameId);
+  
 
   return (
     <DefaultLayout>
-      <h2>{gameIndex.playerName}さん、夜の行動を行ってください</h2>
-      あなたの役職は{gameIndex.playerRole.roleName}です。
-      ほかのプレイヤーは以下の通りです。
-      <ul>
-        {gameIndex.otherPlayerList.map((player) => (
-          <li key={player.id}>{player.name}</li>
-        ))}
-      </ul>
+      <h2 className={styles.title}>
+        {gameIndex.playerName}さん、夜の行動を行ってください
+      </h2>
+
+      <RoleDescription roleId={gameIndex.playerRole.roleId} />
+
+      <NightActionForm
+        doneNightAct={doneNightAct}
+        playerRole={gameIndex.playerRole}
+        otherPlayerList={gameIndex.otherPlayerList}
+      />
     </DefaultLayout>
   );
 };

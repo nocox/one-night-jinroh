aab396dbce98bfe1cbe68fe790af6fba66a20f03
c501306014 <c501306014@gmail.com>
2023-09-25 19:56:31 +0900

    Loading コンポーネントを追加

--- react/src/components/index.ts
+++ react/src/components/index.ts
@@ -1,3 +1,4 @@
 export { DefaultLayout } from './layout/DefaultLayout/DefaultLayout';
 export { Button } from './Button';
 export { ContentBox } from './ContentBox';
+export { Loading } from './Loading';

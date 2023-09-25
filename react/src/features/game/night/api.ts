7646777a819dfc7c2d029c61961d2a8dd56ea568
c501306014 <c501306014@gmail.com>
2023-09-25 19:59:09 +0900

    night のAPIリクエスト処理に怪盗の行動のPOST/GET処理を追加

--- react/src/features/game/night/api.ts
+++ react/src/features/game/night/api.ts
@@ -1,5 +1,5 @@
-import type { FetchNightIndex, NightIndexResponseBody } from './type';
-import { isNightIndexResponseBody } from './type';
+import type { FetchNightIndex, FetchNightKaitoActionResult, NightIndexResponseBody, NightKaitoResult, PostNightKaitoAction } from './type';
+import { isNightIndexResponseBody, isNightKaitoResult } from './type';
 import { JINROH_API_BASE_URL } from '@/url';
 
 export const fetchNightIndex: FetchNightIndex = async () => {
@@ -16,3 +16,46 @@ export const fetchNightIndex: FetchNightIndex = async () => {
 
   return data;
 };
+
+export const postNightKaitoAction: PostNightKaitoAction = async (dto) => {
+  const { participantId } = dto;
+
+  const response = await fetch(JINROH_API_BASE_URL + '/night/kaito', {
+    method: 'POST',
+    credentials: 'include',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ participantId }),
+  })
+
+  const data = (await response.json()) as NightKaitoResult;
+
+  if (!isNightKaitoResult(data)) {
+    throw new Error('APIのレスポンス形式が不正です');
+  }
+
+  return data;
+}
+
+export const fetchNightKaitoActionResult: FetchNightKaitoActionResult | undefined = async () => {
+
+  const response = await fetch(JINROH_API_BASE_URL + '/night/kaito', {
+    method: 'GET',
+    credentials: 'include',
+
+  });
+
+
+  const data = (await response.json()) as NightKaitoResult | null;
+
+  if (data === null) {
+    return undefined;
+  }
+
+  if (!isNightKaitoResult(data)) {
+    throw new Error('APIのレスポンス形式が不正です');
+  }
+
+  return data;
+}

bbe1772d41287eb3271a1a593f71817ed25d1fbb
c501306014 <c501306014@gmail.com>
2023-09-25 20:26:58 +0900

    night page に RoleDescriptionコンポーネントを実装

---
+++ react/src/features/game/night/components/RoleDescription/RoleDescription.tsx
@@ -0,0 +1,18 @@
+import { RoleDescriptionBox } from './RoleDescriptionBox';
+import { characters } from '@/features/game/character';
+
+type Props = {
+  roleId: number;
+};
+
+export const RoleDescription: React.FC<Props> = ({ roleId }) => {
+  // console.log('roleId', roleId);
+
+  const character = characters.find((c) => c.roleId === roleId);
+  if (!character) {
+    // TODO エラー画面を表示する. 必要に応じてカスタムエラークラスを作成する.
+    throw new Error('character not found');
+  }
+
+  return <RoleDescriptionBox character={character} />;
+};

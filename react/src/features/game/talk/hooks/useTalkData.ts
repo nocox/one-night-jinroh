55e0d7f5190aa3edbd2f534802ba899a84bb096e
c501306014 <c501306014@gmail.com>
2023-09-30 20:57:01 +0900

    :sparkles: useTalkDataがレスポンスデータを加工して値を返すようにした

--- react/src/features/game/talk/hooks/useTalkData.ts
+++ react/src/features/game/talk/hooks/useTalkData.ts
@@ -1,20 +1,38 @@
 import { useEffect, useState } from 'react';
 import { fetchTalkIndex } from '../api';
-import type { TalkIndexResponseBody } from '../type';
+import type { Player } from '../type';
 
-export const useTalkData = (): TalkIndexResponseBody | undefined => {
-  const [talkIndexResponseBody, setTalkIndexResponseBody] = useState<
-    TalkIndexResponseBody | undefined
-  >(undefined);
+export const useTalkData = (): {
+  gameId: number | undefined;
+  players: Player[] | undefined;
+} => {
+  const [players, setPlayers] = useState<Player[] | undefined>(undefined);
+  const [gameId, setGameId] = useState<number | undefined>(undefined);
 
   useEffect(() => {
     const fetchTalkIndexAsync = async () => {
       const talkIndexResponseBody = await fetchTalkIndex();
-      setTalkIndexResponseBody(talkIndexResponseBody);
+      const { gameIndex, cos } = talkIndexResponseBody;
+      const players: Player[] = [
+        {
+          id: gameIndex.playerId,
+          name: gameIndex.playerName,
+          role: gameIndex.playerRole,
+          co: cos.find((co) => co.id === gameIndex.playerId)!,
+        },
+        ...gameIndex.otherPlayerList.map((otherPlayer) => ({
+          id: otherPlayer.id,
+          name: otherPlayer.name,
+          role: otherPlayer.role,
+          co: cos.find((co) => co.id === otherPlayer.id)!,
+        })),
+      ];
+      setPlayers(players);
+      setGameId(talkIndexResponseBody.gameId);
     };
 
     void fetchTalkIndexAsync();
   }, []);
 
-  return talkIndexResponseBody;
+  return { gameId, players };
 };

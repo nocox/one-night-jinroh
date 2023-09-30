9145744ede984c45f558e5fcfcdcd0b22eb66a3c
c501306014 <c501306014@gmail.com>
2023-09-30 19:02:51 +0900

    :sparkles: talkIndexを取得するカスタムフックを実装

---
+++ react/src/features/game/talk/hooks/useTalkData.ts
@@ -0,0 +1,21 @@
+import { useEffect, useState } from 'react';
+import { fetchTalkIndex } from '../api';
+import type { TalkIndexResponseBody } from '../type';
+
+export const useTalkData = (): TalkIndexResponseBody | undefined => {
+  const [talkIndexResponseBody, setTalkIndexResponseBody] = useState<
+    TalkIndexResponseBody | undefined
+  >(undefined);
+
+
+  useEffect(() => {
+    const fetchTalkIndexAsync = async () => {
+      const talkIndexResponseBody = await fetchTalkIndex();
+      setTalkIndexResponseBody(talkIndexResponseBody);
+    };
+
+    void fetchTalkIndexAsync();
+  }, []);
+
+  return talkIndexResponseBody;
+};

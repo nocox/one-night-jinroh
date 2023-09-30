cbf5593338d2a75e9a0bddea6771bec7c8cfa449
c501306014 <c501306014@gmail.com>
2023-09-30 19:02:05 +0900

    :sparkles: 話し合いページ到達時のデータフェッチ処理を実装

---
+++ react/src/features/game/talk/type.ts
@@ -0,0 +1,36 @@
+import { z } from "zod";
+import { gameIndexSchema } from "@/features/game/type";
+import type { GameIndex } from "@/features/game/type";
+
+/**
+ * 話し合いページ読み込み時のレスポンス
+ */
+type Co = {
+  id: number;
+  role: string;
+};
+
+export type TalkIndexResponseBody = {
+  gameId: number;
+  gameIndex: GameIndex;
+  cos: Co[];
+};
+
+const coSchema = z.object({
+  id: z.number(),
+  role: z.string(),
+});
+
+const talkIndexResponseBodySchema = z.object({
+  gameId: z.number(),
+  gameIndex: gameIndexSchema,
+  cos: z.array(coSchema),
+});
+
+export const isTalkIndexResponseBody = (
+  value: unknown,
+): value is TalkIndexResponseBody => {
+  return talkIndexResponseBodySchema.safeParse(value).success;
+};
+
+export type FetchTalkIndex = () => Promise<TalkIndexResponseBody>;

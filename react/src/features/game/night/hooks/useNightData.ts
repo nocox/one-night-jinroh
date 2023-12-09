0b35b013dfc3a26d162fe999f9b15f157fb2dcd4
c501306014 <c501306014@gmail.com>
2023-12-09 14:56:39 +0900

    :fire: useNightData からGameIndexに関する取得処理を削除

--- react/src/features/game/night/hooks/useNightData.ts
+++ react/src/features/game/night/hooks/useNightData.ts
@@ -1,27 +1,23 @@
 import { useEffect, useState } from 'react';
 import { fetchNightIndex } from '@/features/game/night/api';
-import type { GameIndex } from '@/features/game/type';
 
 export const useNightData = (): {
   gameId: number | undefined;
-  gameIndex: GameIndex | undefined;
   doneNightAct: boolean | undefined;
 } => {
   const [gameId, setGameId] = useState<number | undefined>(undefined);
-  const [gameIndex, setGameIndex] = useState<GameIndex | undefined>(undefined);
   const [doneNightAct, setDoneNightAct] = useState<boolean | undefined>(
     undefined,
   );
 
   useEffect(() => {
     const fetchNightIndexAsync = async () => {
-      const { gameId, gameIndex, doneNightAct } = await fetchNightIndex();
+      const { gameId, doneNightAct } = await fetchNightIndex();
       setGameId(gameId);
-      setGameIndex(gameIndex);
       setDoneNightAct(doneNightAct);
     };
     void fetchNightIndexAsync();
   }, []);
 
-  return { gameId, gameIndex, doneNightAct };
+  return { gameId, doneNightAct };
 };

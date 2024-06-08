3ec2c83b1bf71707e1b9af60602f4b0d3cb45f4a
kuniyuki <c501306014@gmail.com>
2024-06-08 16:33:20 +0900

    :recycle: useTalkData.ts をリファクタリングした

--- react/src/features/game/talk/hooks/useTalkData.ts
+++ react/src/features/game/talk/hooks/useTalkData.ts
@@ -1,10 +1,16 @@
+import type React from 'react';
 import { useCallback, useEffect, useState } from 'react';
 import { fetchTalkIndex } from '../api';
 import { UnexpectedError } from '@/features/error';
 import { useGameIndex } from '@/features/game/hooks/useGameIndex';
-import type { CoRole, GameParticipantWithCoRole } from '@/features/game/type';
+import type {
+  CoRole,
+  GameParticipant,
+  GameParticipantWithCoRole,
+  RoleBean,
+} from '@/features/game/type';
 
-export const useTalkData = (): {
+type UseTalkData = () => {
   gameId: number | undefined;
   nightActLog: string | undefined;
   hostFlag: boolean | undefined;
@@ -13,21 +19,26 @@ export const useTalkData = (): {
     React.SetStateAction<GameParticipantWithCoRole[] | undefined>
   >;
   getMyPlayer: () => GameParticipantWithCoRole;
-} => {
+};
+
+export const useTalkData: UseTalkData = () => {
   const [gameId, setGameId] = useState<number | undefined>(undefined);
   const [cos, setCos] = useState<CoRole[] | undefined>(undefined);
   const [gameParticipantsWithCoRole, setGameParticipantsWithCoRole] = useState<
     GameParticipantWithCoRole[] | undefined
   >(undefined);
 
+  // gameId と cos を取得
   useEffect(() => {
-    const fetchTalkIndexAsync = async () => {
-      const talkIndexResponseBody = await fetchTalkIndex();
-      setGameId(talkIndexResponseBody.gameId);
-      setCos(talkIndexResponseBody.cos);
-    };
-
-    void fetchTalkIndexAsync();
+    void (async () => {
+      try {
+        const talkIndexResponseBody = await fetchTalkIndex();
+        setGameId(talkIndexResponseBody.gameId);
+        setCos(talkIndexResponseBody.cos);
+      } catch (e) {
+        console.error(e);
+      }
+    })();
   }, []);
 
   const {
@@ -39,6 +50,7 @@ export const useTalkData = (): {
     hostFlag,
   } = useGameIndex('talk', gameId);
 
+  // gameParticipantsWithCoRole を生成
   useEffect(() => {
     if (
       hostFlag === undefined ||
@@ -50,25 +62,21 @@ export const useTalkData = (): {
     )
       return;
 
-    const gameParticipantsWithCoRole: GameParticipantWithCoRole[] = [
-      {
-        hostFlag,
-        id: playerId,
-        name: playerName,
-        role: playerRole,
-        co: cos.find((co) => co.id === playerId)!,
-      },
-      ...otherPlayerList.map((otherPlayer) => ({
-        hostFlag: otherPlayer.hostFlag,
-        id: otherPlayer.id,
-        name: otherPlayer.name,
-        role: otherPlayer.role,
-        co: cos.find((co) => co.id === otherPlayer.id)!,
-      })),
-    ];
+    const gameParticipantsWithCoRole = toGameParticipantsWithCoRole(
+      otherPlayerList,
+      hostFlag,
+      playerId,
+      playerName,
+      playerRole,
+      cos,
+    );
+
     setGameParticipantsWithCoRole(gameParticipantsWithCoRole);
   }, [playerId, playerName, playerRole, otherPlayerList, cos, hostFlag]);
 
+  /**
+   * 自分のプレイヤー情報を取得
+   */
   const getMyPlayer = useCallback((): GameParticipantWithCoRole => {
     if (!gameParticipantsWithCoRole) {
       throw new UnexpectedError('players is undefined');
@@ -85,4 +93,33 @@ export const useTalkData = (): {
     setGameParticipantsWithCoRole,
     getMyPlayer,
   };
+};
+
+/**
+ * 参加者にCoRoleを付与したGameParticipantWithCoRoleの配列を返す
+ */
+const toGameParticipantsWithCoRole = (
+  otherPlayerList: GameParticipant[],
+  hostFlag: boolean,
+  playerId: number,
+  playerName: string,
+  playerRole: RoleBean,
+  cos: CoRole[],
+): GameParticipantWithCoRole[] => {
+  return [
+    {
+      hostFlag,
+      id: playerId,
+      name: playerName,
+      role: playerRole,
+      co: cos.find((co) => co.id === playerId)!,
+    },
+    ...otherPlayerList.map((otherPlayer) => ({
+      hostFlag: otherPlayer.hostFlag,
+      id: otherPlayer.id,
+      name: otherPlayer.name,
+      role: otherPlayer.role,
+      co: cos.find((co) => co.id === otherPlayer.id)!,
+    })),
+  ];
 };

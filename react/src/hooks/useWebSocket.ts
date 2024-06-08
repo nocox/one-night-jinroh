307e92ca19352d5cbbb5802b6d68a6c520424749
nocox <ddnd7nocox@gmail.com>
2024-06-08 16:37:10 +0900

    WebSocketの接続が切れた場合に、再接続を促すconfirmを出すように修正した。

--- react/src/hooks/useWebSocket.ts
+++ react/src/hooks/useWebSocket.ts
@@ -7,7 +7,8 @@ import { JINROH_API_BASE_URL } from '@/url';
 export const useWebSocket = (callbacks: Subscribe[]): void => {
   useEffect(() => {
     const socket = new SockJS(JINROH_API_BASE_URL + '/jinroh-websocket');
-    const stompClient = Stomp.over(socket);
+    const options = {debug: true, protocols: Stomp.VERSIONS.supportedProtocols() };
+    const stompClient = Stomp.over(socket, options);
 
     stompClient.connect({}, () => {
       callbacks.forEach((callback) => {
@@ -15,11 +16,17 @@ export const useWebSocket = (callbacks: Subscribe[]): void => {
       });
     });
 
+    socket.onclose = () => {
+      if (confirm("サーバとの接続が切れました。再接続しますか？")) {
+        location.reload()
+      }
+    };
+
     return () => {
       if (stompClient.connected) {
         stompClient.disconnect();
+        socket.close();
       }
-      socket.close();
     };
   }, [callbacks]);
 };

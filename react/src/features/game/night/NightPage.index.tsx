abf82dfa2ca2f6db1e71eca1459fa2d43cd6b773
kuniyuki <c501306014@gmail.com>
2024-09-28 15:38:47 +0900

    feat(react-error-handling): 夜の行動ページに試しにエラーハンドリングを導入

--- react/src/features/game/night/NightPage.index.tsx
+++ react/src/features/game/night/NightPage.index.tsx
@@ -1,7 +1,13 @@
 import { createRoot } from 'react-dom/client';
+import { ErrorBoundary } from 'react-error-boundary';
+import { ErrorFallback } from '@/features/error/ErrorFallback';
 import { NightPage } from '@/features/game/night/NightPage';
 
 const container = document.getElementById('jsi-entry');
 const root = createRoot(container!);
 
-root.render(<NightPage />);
+root.render(
+  <ErrorBoundary fallback={<ErrorFallback />}>
+    <NightPage />
+  </ErrorBoundary>,
+);

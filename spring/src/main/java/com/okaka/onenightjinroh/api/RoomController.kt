bd1cfcd9bafe4ee914c679bce0b5736675c21138
nocox <ddnd7nocox@gmail.com>
2024-11-02 16:28:28 +0900

    RoomControllerをkotlin化

--- spring/src/main/java/com/okaka/onenightjinroh/api/RoomController.kt
+++ spring/src/main/java/com/okaka/onenightjinroh/api/RoomController.kt
@@ -1,67 +1,49 @@
-package com.okaka.onenightjinroh.api;
+package com.okaka.onenightjinroh.api
 
-import com.okaka.onenightjinroh.application.domain.Room;
-import com.okaka.onenightjinroh.application.exception.RoomNotExistException;
-import com.okaka.onenightjinroh.application.service.room.*;
-import com.okaka.onenightjinroh.application.validater.ExistRoomValidate;
-import com.okaka.onenightjinroh.application.validater.StartGameValidate;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.View;
-
-import javax.servlet.http.HttpSession;
+import com.okaka.onenightjinroh.application.service.room.*
+import com.okaka.onenightjinroh.application.validater.ExistRoomValidate
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.messaging.simp.SimpMessagingTemplate
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.servlet.View
+import javax.servlet.http.HttpSession
 
 @RestController
-public class RoomController {
+class RoomController(
+private val session: HttpSession,
+private val messagingTemplate: SimpMessagingTemplate,
+private var getRoomIndexUseCase: GetRoomIndexUseCase,
+private val existRoomValidate: ExistRoomValidate,
+private val startGameUseCase: StartGameUseCase,
+) {
 
-    private static final Logger log = LoggerFactory.getLogger(RoomController.class);
-    @Autowired
-    HttpSession session;
+    @get:RequestMapping(path = ["/room-index"])
+    val room: RoomIndexBean
+        get() {
+            val uuid = session.getAttribute("room_uuid").toString()
+            val strUserId = session.getAttribute("user_id").toString()
+            val userId = strUserId.toLong()
 
-    @Autowired
-    private SimpMessagingTemplate messagingTemplate;
+            return getRoomIndexUseCase.getRoomIndex(userId, uuid)
+        }
 
-    @Autowired
-    GetRoomIndexUseCase getRoomIndexUseCase;
+    @RequestMapping(path = ["/game-start"])
+    fun startGame(): String {
+        val uuid = session.getAttribute("room_uuid").toString()
+        val userId = session.getAttribute("user_id").toString().toLong()
 
-    @Autowired
-    private ExistRoomValidate existRoomValidate;
-
-    @Autowired
-    private StartGameUseCase startGameUseCase;
-
-    @Autowired
-    private FinishRoomUseCase finishRoomUseCase;
-    @Autowired
-    private View error;
-
-    @RequestMapping(path = "/room-index")
-    RoomIndexBean getRoom() {
-        String uuid = session.getAttribute("room_uuid").toString();
-        String strUserId = session.getAttribute("user_id").toString();
-        Long userId = Long.valueOf(strUserId);
-
-        return getRoomIndexUseCase.getRoomIndex(userId, uuid);
-    }
-
-    @RequestMapping(path = "/game-start")
-    String startGame() {
-        String uuid = session.getAttribute("room_uuid").toString();
-        Long userId = Long.valueOf(session.getAttribute("user_id").toString());
-
-        Room room = existRoomValidate.existRoom(uuid).orElseThrow(IllegalArgumentException::new);
+        val room = existRoomValidate.existRoom(uuid).orElseThrow { IllegalArgumentException() }
 
         try {
-            GameStartWebSocketBean gameStartWebSocketBean = startGameUseCase.startGame(room.getRoomId(), userId);
+            val gameStartWebSocketBean = startGameUseCase.startGame(room.getRoomId(), userId)
             // ここでブロードキャストする
-            messagingTemplate.convertAndSend("/topic/" + room.uuid, gameStartWebSocketBean);
-            return "SUCCESS";
-        } catch (NotEnoughParticipantsException e) {
-            return "NOT_ENOUGH_PARTICIPANTS";
+            messagingTemplate.convertAndSend("/topic/" + room.uuid, gameStartWebSocketBean)
+            return "SUCCESS"
+        } catch (e: NotEnoughParticipantsException) {
+            return "NOT_ENOUGH_PARTICIPANTS"
         }
     }
 }

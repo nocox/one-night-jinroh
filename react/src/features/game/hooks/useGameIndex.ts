46126984ecaa2d39f52db0e7b79795e7bc344c80
kuniyuki <c501306014@gmail.com>
2024-12-14 16:40:03 +0900

    refactor: useGameIndexのフェッチ処理をuseQueryに置き換え

--- react/src/features/game/hooks/useGameIndex.ts
+++ react/src/features/game/hooks/useGameIndex.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
 import { fetchGetWrapper } from '@/api';
 import {
   type GameParticipant,
@@ -17,63 +17,40 @@ export const useGameIndex = (
   playerId: number | undefined;
   playerName: string | undefined;
   playerRole: RoleBean | undefined;
-  error: Error | undefined;
+  error: Error | null;
 } => {
-  const [hostFlag, setHostFlag] = useState<boolean | undefined>(undefined);
-  const [nightActLog, setNightActLog] = useState<string | undefined>(undefined);
-  const [otherPlayerList, setGameParticipantList] = useState<
-    GameParticipant[] | undefined
-  >(undefined);
-  const [playerId, setPlayerId] = useState<number | undefined>(undefined);
-  const [playerName, setPlayerName] = useState<string | undefined>(undefined);
-  const [playerRole, setPlayerRole] = useState<RoleBean | undefined>(undefined);
-  const [error, setError] = useState<Error | undefined>(undefined);
-
-  useEffect(() => {
-    if (gameId === undefined) {
-      console.warn('gameId is undefined');
-
-      return;
-    }
-    const fetchData = async () => {
-      try {
-        const gameIndexResponse = await fetchGetWrapper<GameIndexResponse>(
-          '/game-index',
-          {
-            term: param,
-          },
-        );
-        switch (gameIndexResponse.type) {
-          case 'GameIndex':
-            setHostFlag(gameIndexResponse.hostFlag);
-            setNightActLog(gameIndexResponse.nightActLog ?? '');
-            setGameParticipantList(gameIndexResponse.otherPlayerList);
-            setPlayerId(gameIndexResponse.playerId);
-            setPlayerName(gameIndexResponse.playerName);
-            setPlayerRole(gameIndexResponse.playerRole);
-            break;
-          case 'TermIsDifferent':
-            window.location.href = '/' + gameIndexResponse.term;
-            break;
-          case 'NotStared':
-            window.location.href = '/room';
-            break;
-        }
-      } catch (err) {
-        setError(err as Error);
+  const { data, error } = useQuery({
+    queryKey: ['game-index', gameId],
+    queryFn: async () => {
+      if (gameId !== undefined) {
+        return await fetchGetWrapper<GameIndexResponse>('/game-index', {
+          term: param,
+        });
       }
-    };
+    },
+    enabled: gameId !== undefined,
+  });
 
-    void fetchData();
-  }, [param, gameId]);
+  switch (data?.type) {
+    case 'GameIndex':
+      break;
+    case 'TermIsDifferent':
+      window.location.href = '/' + data.term;
+      break;
+    case 'NotStared':
+      window.location.href = '/room';
+      break;
+  }
 
   return {
-    hostFlag,
-    nightActLog,
-    otherPlayerList,
-    playerId,
-    playerName,
-    playerRole,
+    hostFlag: data?.type === 'GameIndex' ? data.hostFlag : undefined,
+    nightActLog:
+      data?.type === 'GameIndex' ? data.nightActLog ?? '' : undefined,
+    otherPlayerList:
+      data?.type === 'GameIndex' ? data.otherPlayerList : undefined,
+    playerId: data?.type === 'GameIndex' ? data.playerId : undefined,
+    playerName: data?.type === 'GameIndex' ? data.playerName : undefined,
+    playerRole: data?.type === 'GameIndex' ? data.playerRole : undefined,
     error,
   };
 };

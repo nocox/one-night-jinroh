5c6d975a37c1acbefe85c74bae1a56dc2d6c9020
nocox <ddnd7nocox@gmail.com>
2025-03-08 16:17:14 +0900

    GameIdだけ抜き取る実装のリファクタリング

--- react/src/features/game/night/NightPage.tsx
+++ react/src/features/game/night/NightPage.tsx
@@ -1,74 +1,57 @@
 import type React from 'react';
-import {useState} from "react";
 import { NightTemplate } from './NightTemplate';
 import { useNightData } from './hooks/useNightData';
-import {fetchGetWrapper} from "@/api";
 import { Loading } from '@/components';
 import { useGameIndex } from '@/features/game/hooks/useGameIndex';
-import {type FetchGameIdResponse} from "@/features/game/type";
-import {useWebSocketGameWrapper} from "@/hooks";
+import { useWebSocketGameWrapper } from '@/hooks';
+import { GameIdProvider } from '@/components/GameIdProvider';
 import type { Subscribe } from '@/type';
 
-// MEMO: GameIdをゲームページに渡す実装をお試し中。とりあえず夜の行動ページだけ反映中。
 export const NightPage: React.FC = () => {
-    const [gameId, setGameId] = useState<number | undefined>(undefined);
-
-    void fetchGetWrapper<FetchGameIdResponse>('/fetch-game-id').then((res: FetchGameIdResponse) => {
-        if (res.resultCode === "IN_GAME") {
-            setGameId(res.gameId);
-        } else if (res.resultCode === "NOT_IN_GAME") {
-            setGameId(undefined)
-        }
-    })
-
-    return gameId === undefined ? (
-        <div>
-            <p>gameId取得できなかったよ</p>
-
-            <a href="/">Topへ戻る</a>
-        </div>
-    ) : (
-        <InGameNightPage gameId={gameId}/>
-    )
+    return (
+        <GameIdProvider>
+            {(gameId) => <InGameNightPage gameId={gameId} />}
+        </GameIdProvider>
+    );
 };
 
 type Props = {
-  gameId: number
-}
+    gameId: number;
+};
 
 const InGameNightPage: React.FC<Props> = ({gameId}) => {
-  const { doneNightAct } = useNightData();
-  const { playerName, playerRole, otherPlayerList, error } = useGameIndex(
-      'night',
-      gameId,
-  );
+    const { doneNightAct } = useNightData();
+    const { playerName, playerRole, otherPlayerList, error } = useGameIndex(
+        'night',
+        gameId,
+    );
 
-  if (error) {
-    throw error;
-  }
+    if (error) {
+        throw error;
+    }
 
-  const subscribeDoneNightActionOfAllPlayer: Subscribe = {
-    path: `/topic/${gameId}`,
-    callback: () => {
-      window.location.href = '/talk';
-    },
-  };
+    const subscribeDoneNightActionOfAllPlayer: Subscribe = {
+        path: `/topic/${gameId}`,
+        callback: () => {
+            window.location.href = '/talk';
+        },
+    };
 
-  useWebSocketGameWrapper(gameId, [subscribeDoneNightActionOfAllPlayer])
+    useWebSocketGameWrapper(gameId, [subscribeDoneNightActionOfAllPlayer])
 
-  return playerName === undefined ||
-  playerRole === undefined ||
-  otherPlayerList === undefined ||
-  doneNightAct === undefined ? (
-      <Loading />
-  ) : (
-      <>
-        <NightTemplate
-            playerName={playerName}
-            playerRole={playerRole}
-            otherPlayerList={otherPlayerList}
-            doneNightAct={doneNightAct}
-        />
-      </>
-  );
-}
+    return playerName === undefined ||
+        playerRole === undefined ||
+        otherPlayerList === undefined ||
+        doneNightAct === undefined ? (
+        <Loading />
+    ) : (
+        <>
+            <NightTemplate
+                playerName={playerName}
+                playerRole={playerRole}
+                otherPlayerList={otherPlayerList}
+                doneNightAct={doneNightAct}
+            />
+        </>
+    );
+};
